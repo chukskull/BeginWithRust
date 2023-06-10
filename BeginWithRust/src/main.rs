e3b5044b13c01d8d354487eccb2a3d68f527b6ee
Saleh Nagat <snagat@e2r9p1.1337.ma>
2023-06-10 11:49:50 +0100

    first rust program

--- BeginWithRust/src/main.rs
+++ BeginWithRust/src/main.rs
@@ -1,19 +1,41 @@
-use std::io;
+
+// use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Enter your input ");
-
-    let secret :i32 = rand::thread_rng().gen_range(0..=100);
-
-    println!("this is the number : {secret}");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess).expect("fail ");
-    println!("u guess :{guess}");
+    let secret  = rand::thread_rng().gen_range(0..=100);
     
+    loop{
+        println!("Enter your input ");
+
+        let mut guess = String::new();
+        
+        let read = std::io::stdin();
+
+        read.read_line(&mut guess)
+        .expect("fail ");
+
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("u guess :{guess}");
+
+        match guess.cmp(& secret)
+        {
+            Ordering::Less => println!("too low !!"),
+            Ordering::Greater => println!("too big !!"),
+            Ordering::Equal => 
+            {
+                println!("well done");
+                break;
+            }
+        }
+    }
 }
+
